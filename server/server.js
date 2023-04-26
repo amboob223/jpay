ceda847af0b72ec9cc1f7dd02d0ee72c6db29155
amboob223 <monk8896@gmail.com>
2023-04-26 14:17:39 -0400

    hello

--- server/server.js
+++ server/server.js
@@ -6,8 +6,62 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
-const cors = require("cors")
+const cors = require("cors");
+const pool = require("./db");
 
 //middleware 
 app.use(express.json());
-app.use()
+app.use(cors({
+    origin: ["localhost:3000/jpay"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    key: "userID",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24
+    }
+}))
+
+const saltRounds = 10;
+
+const storage = multer.diskStorage({
+    destination: (null, "/images", cb),
+    filename: (null, cb)
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    pool.query(
+        "SELECT * FROM logins WHERE username = ?",
+        username,
+        (err, result) => {
+            if (err) {
+                res.send({ err: err })
+            }
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    if (response) {
+                        req.session.user = result
+                        console.log(req.session.user)
+                        req.send(result)
+                    } else {
+                        res.send({ message: "wrongg username" })
+                    }
+                })
+            }
+        }
+    )
+})
+
+// app.post("/jpay",())
---
+++ server/sql.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE jpay;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    phone VARCHAR (255),
+    bags VARCHAR(255),
+    photo VARCHAR (255),
+    address VARCHAR(255),
+    origin VARCHAR(255),
+    destination VARCHAR(255),
+    payment VARCHAR(255)
+);
+
+CREATE TABLE travelers(
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255),
+    pickup VARCHAR(255),
+    dropoff VARCHAR(255),
+    phone VARCHAR(255),
+    payment VARCHAR(255)
+);
+
+CREATE TABLE logins(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR (255),
+    password BYTEA
+)
